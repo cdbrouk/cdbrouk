525791c9a8b341738e25606060359332bf65000b
brouk <cdbrouk@gmail.com>
2020-03-24 13:25:59 +0900

    Edit about me

--- src/components/about/aboutText.ts
+++ src/components/about/aboutText.ts
@@ -2,14 +2,12 @@ const aboutText = `
   ## Choi hyeongwuk
   JR.FullStack Developer  
   korea / 01026093531\n
-  ### Stack  
-  - Front : React, React-Native, Redux, Typescript
-  - Back: node.js, Python3, Django, GraphQL\n\n
-
-  ### EXPERIENCE
+  ## Stack  
+  -<strong>Front</strong> : React, React-Native, Redux, Typescript  
+  -<strong>Back</strong>: node.js, Python3, Django, GraphQL\n\n
+  ## EXPERIENCE
   <strong>Unichal</strong> - <i>developer (React)</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<small>April 2019 -</small>  
-  <small>present</small>
-  - Create React Native App (O2O service) - <small><i>React, React-native, Python3, DRF</i></small>
-  
+  <small>present</small>  
+  - Create React Native App (O2O service) - <small><i>React, React-native, Python3, DRF</i></small>\n\n
   `;
 export default aboutText;
